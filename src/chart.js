039538c663f75b5b90b15398aaf78cf6e6751de7
willcan <59585577+willcan@users.noreply.github.com>
2020-02-27 16:59:55 -0700

    Update chart.js
    Added functions to the chart and added object

--- src/chart.js
+++ src/chart.js
@@ -17,11 +17,12 @@ import {
 import {Line} from "react-chartjs-2";
 import { Link, Route } from "react-router-dom";
 import { db } from "./firebase";
+import moment from "moment";
 
 export default function Chart(props) {
     const [surveys, setSurveys] = useState ([])
     const [labels, setLabels ] = useState ([])
-    const [datasets, setDataSets] = useState ([])
+    const [dataSets, setDataSets] = useState ([])
 
     useEffect(()=>{
         const unsub = db.collection('users').doc(props.user.uid).collection('surveys').onSnapshot((snapshot)=>{
@@ -38,11 +39,51 @@ export default function Chart(props) {
         })
     },[])
 
+    useEffect(()=>{
+        //get and display labels
+        const lbls = surveys.map((survey)=>{
+            return moment(survey.date).format("M/D/YY")
+        })
+        setLabels(lbls)
+
+        //create a holder array for Data Sets like happiness and sleep 
+        const sets = []
+
+        //creating the sleep object
+        const sleep = {
+            label:"Hours of sleep",
+            data:surveys.map(s => s.sleep),
+            borderColor: 'red',
+            borderWidth:1
+        }
+        sets.push(sleep)
+
+        //Creating happiness objects
+        const happiness = {
+            label:"Happiness",
+            data:surveys.map(s => s.happiness),
+            borderColor: 'blue',
+            borderWidth:1
+        }
+        sets.push(happiness)
+            console.log(sets)
+        //Set the state variable
+        setDataSets(sets)
+
+
+
+    },[surveys])
+
     return(
         <div style={{display: "flex", justifyContent: "center"}}>
             <Paper style={{padding:12, marginTop:30, width: '100%', maxWidth:400}}>
                 <Typography variant ='h4'>Chart</Typography>
-                <Line/>
+                <Line
+                    data={{
+                        labels: labels,
+                        datasets:dataSets
+                    }}
+                />
                 
             </Paper>
         </div>
