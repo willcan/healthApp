4962ceeb139118a165e14a181695b093defd04fa
willcan <59585577+willcan@users.noreply.github.com>
2020-02-29 19:51:45 -0700

    Update chart.js
    Video 2 4 min

--- src/chart.js
+++ src/chart.js
@@ -35,8 +35,18 @@ export default function Chart(props) {
                 }
                 return survey
             })
+
+            const sorted = surveys.sort((a,b)=>{
+                if(a.date > b.date){
+                    return 1
+                } else{
+                    return -1
+                }
+            })
+
             setSurveys(surveys)
         })
+        return unsub
     },[])
 
     useEffect(()=>{
